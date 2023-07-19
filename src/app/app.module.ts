6f401f4d60ed5a6c6f3c4f3d4499cdec081fc800
vpeopleonatank <vpeopleonatank@gmail.com>
2023-07-19 22:00:16 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EMPTY } from 'rxjs';
 
+import { JwtService } from './core/services/jwt.service';
+import { UserService } from './core/services/user.service';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/layout/header.component';
+import { FooterComponent } from './core/layout/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ApiInterceptor } from './core/interceptors/api.interceptor';
+import { TokenInterceptor } from './core/interceptors/token.interceptor';
+
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
+
+export function initAuth(jwtService: JwtService, userService: UserService) {
+  return () => (jwtService.getToken() ? userService.getCurrentUser() : EMPTY);
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderComponent,
+    FooterComponent,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initAuth,
+      deps: [JwtService, UserService],
+      multi: true,
+    },
+    { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+    // { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
